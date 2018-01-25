55815c1310beda3f97b6781165e00fc61227dd53
Administrator <Administrator@zqb-PC>
2018-01-25 17:24:24 +0800

    Minor bug fixes.

--- src/main/java/com/application/controller/Controller.java
+++ src/main/java/com/application/controller/Controller.java
@@ -1,34 +1,30 @@
 package com.application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.application.entity.Bzxm;
 import com.application.entity.BzxmRepository;
 
-@Controller
-public class BzxmController {
-
+@RestController
+public class Controller {
 	@Autowired
 	private BzxmRepository bzxmRepository;
 
-	@RequestMapping(value = "/bzxm", method = RequestMethod.GET)
-	public String getError(Model model, Integer page) {
+	@RequestMapping(value = "/")
+	public Page<Bzxm> restController() {
 
-		// model.addAttribute("bzxm", bzxmRepository.findAll());
-
-		page = null == page ? 0 : page;
-		int size = 20;
+		int page = 0;
+		int size = 5;
 		Sort sort = new Sort(new Sort.Order(Sort.Direction.ASC, "xzqh"), new Sort.Order(Sort.Direction.ASC, "xmmc"));
 
 		Pageable pageable = new PageRequest(page, size, sort);
-		model.addAttribute("page", bzxmRepository.findAll(pageable));
 
-		return "bzxm";
+		return bzxmRepository.findAll(pageable);
 	}
 }
