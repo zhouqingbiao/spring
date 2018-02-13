4509b812cb5b0d66019d882f34ec148768a6ed50
Administrator <Administrator@HOME-PC>
2018-02-13 11:01:27 +0800

    others修改为@RestController

--- src/main/java/com/application/controller/others/BzxmController.java
+++ src/main/java/com/application/controller/others/BzxmController.java
@@ -1,11 +1,11 @@
 package com.application.controller.others;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class BzxmController {
 
 	@RequestMapping(value = "/bzxm", method = { RequestMethod.GET })
--- src/main/java/com/application/controller/others/IndexController.java
+++ src/main/java/com/application/controller/others/IndexController.java
@@ -1,11 +1,11 @@
 package com.application.controller.others;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 	@RequestMapping(value = "/index", method = { RequestMethod.GET })
 	public String indexController(Model model) {
